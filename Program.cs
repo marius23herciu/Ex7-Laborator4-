8ec58581c291ecc490841c6fcc40e605ffac48a2
marius.herciu23 <marius.herciu23@gmail.com>
2022-05-28 11:49:20 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ex7_Laborator4_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex 7 dificultate ridicata
+            //Scrieti o functie recursiva care pentru un numar “n” primit ca parametru, va
+            //afisa urmatoarea piramida a numerelor:
+            //1
+            //2 2
+            //3 3 3
+            //4 4 4 4
+            //…
+            //n n n …..n
+
+            Console.WriteLine("Introduceti un numar:");
+            int numar = int.Parse(Console.ReadLine());
+
+            int start = 1;
+            AfisarePiramida(numar, start);
+        }
+        static int AfisarePiramida(int numar, int start)
+        {
+            while (start>numar)
+            {
+                return numar;
+            }
+            for (int i = 0; i < start; i++)
+            {
+                Console.Write(start + " ");
+            }
+            Console.WriteLine();
+            AfisarePiramida(numar, start + 1);
+            return numar;
+        }
+    }
+}
